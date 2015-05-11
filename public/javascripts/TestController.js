583ad81580ac61e704dd4c83c29b5382540250b3
kopytkoProg <196858@student.pwr.edu.pl>
2015-05-11 12:09:13 +0200

    Com2

---
+++ public/javascripts/TestController.js
@@ -0,0 +1,9 @@
+/**
+ * Created by michal on 2015-05-11.
+ */
+
+
+myApp.controller('TestController', function ($scope)
+{
+    $scope.txt = 'hello'
+});
---
+++ public/javascripts/my_app.js
@@ -0,0 +1,6 @@
+/**
+ * Created by michal on 2015-05-11.
+ */
+var myApp = angular.module('myApp', []);
+
+
